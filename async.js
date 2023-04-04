174065fa2865805872ecf707029b60ef3b581776
mizanmahi <mizanmahi24@gmail.com>
2023-04-05 02:18:10 +0600

    init async example

---
+++ async.js
@@ -0,0 +1,35 @@
+import CryptoJS from 'crypto-js';
+
+export function encryptMessage(message, key, callback) {
+   const encryptedMessage = CryptoJS.AES.encrypt(message, key).toString();
+   callback(encryptedMessage);
+}
+
+export function encryptMessagePromise(message, key) {
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+         const encryptedMessage = CryptoJS.AES.encrypt(message, key).toString();
+         if (encryptedMessage) {
+            resolve(encryptedMessage);
+         } else {
+            reject(new Error('Failed to encrypt message'));
+         }
+      }, 2000);
+   });
+}
+
+// const message = {
+//    name: 'Mizan',
+//    password: '11223344',
+// };
+
+// const secretKey = 'gh#M0CL4ADe%IO59jZ83xuNv';
+
+// encryptMessage(JSON.stringify(message), secretKey, (encryptedMessage) => {
+//    console.log(encryptedMessage);
+// });
+
+// encryptMessagePromise(JSON.stringify(message), secretKey).then(
+//    (encryptedMessage) => console.log(encryptedMessage)
+// );
+
